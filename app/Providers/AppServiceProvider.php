4d6d24c1e3e3382b5335050813f27e62f2fab575
danielaguileta10 <daniel.aguileta@unah.hn>
2022-12-07 14:15:16 -0600

    appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,6 +33,7 @@ class AppServiceProvider extends ServiceProvider
             // This will only accept alpha and spaces. 
             // If you want to accept hyphens use: /^[\pL\s-]+$/u.
             return preg_match('/^[\pL\s]+$/u', $value); 
+            
     
         });
     }
